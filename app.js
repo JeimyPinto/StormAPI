58d24e3eb0f7d83c4fff5782cf2410ec20b4bd19
JeimyPinto <contactojeimypinto@gmail.comm>
2023-11-28 15:56:36 -0500

    empaquetado.

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
-app.listen(3000, () => {
-  console.log('Servidor escuchado por: http://localhost:3000');
+
+// Servir archivos estáticos desde la carpeta node_modules
+app.use("/static", express.static(path.join(__dirname, "node_modules")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+app.listen(3000, () => {
+  console.log("Servidor escuchado por: http://localhost:3000");
 });
