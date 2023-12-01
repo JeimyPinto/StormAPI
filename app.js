2d4a5ddbece43318fad50ca7edc6f60b32c8dec8
JeimyPinto <contactojeimypinto@gmail.comm>
2023-12-01 13:36:10 -0500

    se renderiza el home

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const express = require("express");
-const path = require("path");
-
+const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the 'assets' directory
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/', (req, res) => {
+  res.status(200);
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.listen(3000, () => {
-  console.log("Servidor escuchado por: http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Server escuchado en el puerto http://localhost:${PORT}`);
 });
